852932af405a81fcf01a7189a9c712203f955ab3
Daniel Elkington <danielelkington@me.com>
2016-08-12 14:52:13 +1000

    Added QuantityOnHand to Product table Updated Database Schema to add QuantityOnHand to Product table

--- database/php_sreps.sql
+++ database/php_sreps.sql
@@ -15,9 +15,11 @@ CREATE TABLE Product(
     ProductGroupId int,
     Name varchar(100) NOT NULL,
     Price decimal(8,2),
+    QuantityOnHand int NOT NULL,
     CONSTRAINT FK_Product_ProductGroup FOREIGN KEY (ProductGroupId) 
 	REFERENCES ProductGroup(Id),
-    CONSTRAINT CHK_Price CHECK (Price >= 0)
+    CONSTRAINT CHK_Price CHECK (Price >= 0), #MySQL does not have Check constraints - this would work in other DBMS's though
+    CONSTRAINT CHK_QuantityOnHand CHECK (QuantityOnHand >= 0) #MySQL does not have Check constraints - this would work in other DBMS's though
 );
 
 CREATE TABLE Sale(
@@ -27,7 +29,7 @@ CREATE TABLE Sale(
     SaleDateTime datetime NOT NULL,
     CONSTRAINT FK_Sale_Product FOREIGN KEY (ProductId) 
     REFERENCES Product(Id),
-    CONSTRAINT CHK_Quantity CHECK (Quantity >= 1)
+    CONSTRAINT CHK_Quantity CHECK (Quantity >= 1) #MySQL does not have Check constraints - this would work in other DBMS's though
 );
 
 #Insert static reference data
@@ -38,27 +40,27 @@ INSERT INTO ProductGroup(Name) VALUES('Fragrances');
 INSERT INTO ProductGroup(Name) VALUES('Weight loss');
 INSERT INTO ProductGroup(Name) VALUES('Dental care');
 
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol 500mg 100 tablets', 9.99);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol 500mg 50 tablets', 6.99);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol Rapid 20 tablets', 4.49);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Nurofen 200mg 96 tablets', 15.99);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Nurofen for Children 1-5 Years Strawberry', 17.99);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Prescription drugs'), 'Lipitor 20mg Tablets 30', 6.99);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Prescription drugs'), 'Plavix 75mg Tablets 28 (a)', 7.29);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Vitamins'), 'Swisse Ultiboost Calcium + Vitamin D', 13.00);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Vitamins'), 'Herron Osteo Eze Active Plus MSM 120 Tablets', 40.83);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES(NULL, 'Blackmores Odourless Fish Oil 1000mg Bulk Pack 500 Capsules', 38.20);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES(NULL, 'Ansell Glove Hany Disposable 24', 3.99);
-INSERT INTO Product(ProductGroupId, Name, Price) 
-VALUES(NULL, 'Morning Fresh Dishwashing Liquid Lemon 400ml', 1.50);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand) 
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol 500mg 100 tablets', 9.99, 400);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol 500mg 50 tablets', 6.99, 250);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol Rapid 20 tablets', 4.49, 382);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Nurofen 200mg 96 tablets', 15.99, 382);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Nurofen for Children 1-5 Years Strawberry', 17.99, 37);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Prescription drugs'), 'Lipitor 20mg Tablets 30', 6.99, 21);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Prescription drugs'), 'Plavix 75mg Tablets 28 (a)', 7.29, 3);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Vitamins'), 'Swisse Ultiboost Calcium + Vitamin D', 13.00, 421);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Vitamins'), 'Herron Osteo Eze Active Plus MSM 120 Tablets', 40.83, 21);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES(NULL, 'Blackmores Odourless Fish Oil 1000mg Bulk Pack 500 Capsules', 38.20, 7);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES(NULL, 'Ansell Glove Hany Disposable 24', 3.99, 21);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
+VALUES(NULL, 'Morning Fresh Dishwashing Liquid Lemon 400ml', 1.50, 20);
