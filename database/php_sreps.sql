54110e02d119069565e2db0d3dcbc2e274ba4e90
Daniel Elkington <danielelkington@me.com>
2016-08-14 21:30:32 +1000

    Adding SaleLine, Customer tables Changes to the database schema based on advice from Danielle (see Slack conversation August 12th). Creating the SaleLine table to allow multiple things sold in a single transaction to be linked as a single 'sale', and adding the customer table to allow sales to potentially be linked to customers in the future. Also adding more columns to the Product Table which may be useful in terms of tracking when orders need to be made, and the quantity that needs to be ordered.

--- database/php_sreps.sql
+++ database/php_sreps.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS php_SRePS;
 USE php_SRePS;
 
-DROP TABLE IF EXISTS Sale, Product, ProductGroup;
+DROP TABLE IF EXISTS SaleLine, Sale, Customer, Product, ProductGroup;
 
 #Create tables
 
@@ -16,19 +16,35 @@ CREATE TABLE Product(
     Name varchar(100) NOT NULL,
     Price decimal(8,2),
     QuantityOnHand int NOT NULL,
-    CONSTRAINT FK_Product_ProductGroup FOREIGN KEY (ProductGroupId) 
-	REFERENCES ProductGroup(Id),
+    QuantitySold int NOT NULL,
+    QuantityToOrder int NOT NULL,
+    QuantityRequested int NOT NULL,
+    CONSTRAINT FK_Product_ProductGroup FOREIGN KEY (ProductGroupId) REFERENCES ProductGroup(Id),
     CONSTRAINT CHK_Price CHECK (Price >= 0), #MySQL does not have Check constraints - this would work in other DBMS's though
     CONSTRAINT CHK_QuantityOnHand CHECK (QuantityOnHand >= 0) #MySQL does not have Check constraints - this would work in other DBMS's though
 );
 
+CREATE TABLE Customer(
+	Id int PRIMARY KEY AUTO_INCREMENT,
+    FirstName varchar(50) NOT NULL,
+    FamilyName varchar(50) NOT NULL,
+    PhoneNumber varchar(10)
+);
+
 CREATE TABLE Sale(
 	Id int PRIMARY KEY AUTO_INCREMENT,
-    ProductId int NOT NULL,
-    Quantity int NOT NULL,
+    CustomerId int,
     SaleDateTime datetime NOT NULL,
-    CONSTRAINT FK_Sale_Product FOREIGN KEY (ProductId) 
-    REFERENCES Product(Id),
+    CONSTRAINT Sale_Customer FOREIGN KEY (CustomerId) REFERENCES Customer(Id)
+);
+
+CREATE TABLE SaleLine(
+	Id int PRIMARY KEY AUTO_INCREMENT,
+    ProductId int NOT NULL,
+    SaleId int NOT NULL,
+    Quantity int NOT NULL,
+    CONSTRAINT FK_SaleLine_Product FOREIGN KEY (ProductId) REFERENCES Product(Id),
+    CONSTRAINT FK_SaleLine_Sale FOREIGN KEY (SaleId) REFERENCES Sale(Id),
     CONSTRAINT CHK_Quantity CHECK (Quantity >= 1) #MySQL does not have Check constraints - this would work in other DBMS's though
 );
 
@@ -40,27 +56,27 @@ INSERT INTO ProductGroup(Name) VALUES('Fragrances');
 INSERT INTO ProductGroup(Name) VALUES('Weight loss');
 INSERT INTO ProductGroup(Name) VALUES('Dental care');
 
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand) 
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol 500mg 100 tablets', 9.99, 400);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol 500mg 50 tablets', 6.99, 250);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol Rapid 20 tablets', 4.49, 382);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Nurofen 200mg 96 tablets', 15.99, 382);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Nurofen for Children 1-5 Years Strawberry', 17.99, 37);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Prescription drugs'), 'Lipitor 20mg Tablets 30', 6.99, 21);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Prescription drugs'), 'Plavix 75mg Tablets 28 (a)', 7.29, 3);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Vitamins'), 'Swisse Ultiboost Calcium + Vitamin D', 13.00, 421);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Vitamins'), 'Herron Osteo Eze Active Plus MSM 120 Tablets', 40.83, 21);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES(NULL, 'Blackmores Odourless Fish Oil 1000mg Bulk Pack 500 Capsules', 38.20, 7);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES(NULL, 'Ansell Glove Hany Disposable 24', 3.99, 21);
-INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand)
-VALUES(NULL, 'Morning Fresh Dishwashing Liquid Lemon 400ml', 1.50, 20);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol 500mg 100 tablets', 9.99, 400, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol 500mg 50 tablets', 6.99, 250, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Panadol Rapid 20 tablets', 4.49, 382, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Nurofen 200mg 96 tablets', 15.99, 382, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Painkillers'), 'Nurofen for Children 1-5 Years Strawberry', 17.99, 37, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Prescription drugs'), 'Lipitor 20mg Tablets 30', 6.99, 21, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Prescription drugs'), 'Plavix 75mg Tablets 28 (a)', 7.29, 3, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Vitamins'), 'Swisse Ultiboost Calcium + Vitamin D', 13.00, 421, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES((SELECT Id FROM ProductGroup WHERE Name = 'Vitamins'), 'Herron Osteo Eze Active Plus MSM 120 Tablets', 40.83, 21, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES(NULL, 'Blackmores Odourless Fish Oil 1000mg Bulk Pack 500 Capsules', 38.20, 7, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES(NULL, 'Ansell Glove Hany Disposable 24', 3.99, 21, 0, 0, 0);
+INSERT INTO Product(ProductGroupId, Name, Price, QuantityOnHand, QuantitySold, QuantityToOrder, QuantityRequested) 
+VALUES(NULL, 'Morning Fresh Dishwashing Liquid Lemon 400ml', 1.50, 20, 0, 0, 0);
