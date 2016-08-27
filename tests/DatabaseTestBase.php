98d14c936232d58283e736a870034af2b3975607
Daniel Elkington <danielelkington@me.com>
2016-08-28 08:02:17 +1000

    Fixing TravisCI database testing issue regarding truncating tables When TravisCI runs phpunit database tests, it uses a version of MySQL and PHPUnit that doesn't allow you to truncate tables that have foreign keys. Because our tables have foreign key constraints it therefore fails (this doesn't appear to be an issue when running on a local virtual machine). Solution is to override a class and a method in the Database Testing base class to tell phpunit to set foreign key checks off before running the tests, perform the truncate operation, and then set foreign key checks back on.

--- tests/DatabaseTestBase.php
+++ tests/DatabaseTestBase.php
@@ -1,5 +1,21 @@
 <?php
 
+//Small hack: when PHPUnit attempts to truncate tables before running tests
+//on TravisCI, it fails due to foreign key checks preventing the truncation.
+//To fix this, we'll turn off foreign key checks before truncating,
+//and turn them back on afterwards.
+//Fix found here http://stackoverflow.com/a/10331869/5740181 accessed 28/08/2016
+class TruncateOperation extends \PHPUnit_Extensions_Database_Operation_Truncate
+{
+	public function execute(\PHPUnit_Extensions_Database_DB_IDatabaseConnection $connection,
+						 \PHPUnit_Extensions_Database_DataSet_IDataSet $dataSet)
+    {
+    	$connection->getConnection()->query("SET foreign_key_checks = 0");
+    	parent::execute($connection, $dataSet);
+    	$connection->getConnection()->query("SET foreign_key_checks = 1");
+    }
+}
+
 abstract class DatabaseTestBase extends PHPUnit_Extensions_Database_TestCase
 {
 	//Only instantiate pdo once for test clean-up/fixture load
@@ -28,6 +44,15 @@ abstract class DatabaseTestBase extends PHPUnit_Extensions_Database_TestCase
 	public function getDataSet()
 	{
 		return $this->createMySQLXMLDataSet('file.xml');
+	}
+
+	//Hacking around the truncate issue by telling PHPUnit to use our special 'TruncateOpeartion'
+	//class whenever it does its truncates before running tests.
+	public function getSetUpOperation()
+	{
+		$cascadeTruncates = false;
+
+		return new \PHPUnit_Extensions_Database_Operation_Composite(array(new TruncateOperation($cascadeTruncates), \PHPUnit_Extensions_Database_Operation_Factory::INSERT()));
 	}
 
 	protected function WriteToDatabase($query, &$insertID = NULL)
