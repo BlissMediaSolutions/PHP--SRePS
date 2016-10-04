20a6f737f89040ab02d842ff68127b61a8e29cf1
Daniel Elkington <danielelkington@me.com>
2016-10-04 20:31:18 +1100

    Validating sales, including product quantity on hand

--- js/app.js
+++ js/app.js
@@ -227,6 +227,38 @@ app.controller('SalesController', function($scope, $http, $routeParams, $locatio
     $scope.readyPage();
 });
 
+function isEmpty(value) {
+  return angular.isUndefined(value) || value === '' || value === null || value !== value;
+}
+
+//The default ng-max validation attribute with Angular doesn't allow you to bind to 
+//something in your Controller, this should fix it.
+//From http://jsfiddle.net/g/s5gKC/ accessed 4/10/2016
+app.directive('ngMax', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, elem, attr, ctrl) {
+            scope.$watch(attr.ngMax, function(){
+                ctrl.$setViewValue(ctrl.$viewValue);
+            });
+            var maxValidator = function(value) {
+              var max = scope.$eval(attr.ngMax) || Infinity;
+              if (!isEmpty(value) && value > max) {
+                ctrl.$setValidity('ngMax', false);
+                return undefined;
+              } else {
+                ctrl.$setValidity('ngMax', true);
+                return value;
+              }
+            };
+
+            ctrl.$parsers.push(maxValidator);
+            ctrl.$formatters.push(maxValidator);
+        }
+    };
+});
+
 app.filter('startFrom', function () {
     return function (data, start) {
         return data.slice(start);
